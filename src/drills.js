aa0258c6e6e4ac444f21996ce869007e6ae176e2
nrrao <nandanarao@yahoo.com>
2019-10-01 12:02:06 -0400

    finished drills

---
+++ src/drills.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const knex=require('knex');
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+function searchByName(searchTerm) {
+  knexInstance
+    .select('id', 'name', 'price', 'category')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+searchByName('fish');
+
+function paginateProducts(page) {
+  const productsPerPage = 6;
+  const offset = productsPerPage * (page - 1);
+  knexInstance
+    .select('id', 'name', 'price', 'category')
+    .from('shopping_list')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result);
+    });
+}
+
+paginateProducts(2);
+
+function itemsAddedAfterDate(dayAgo){
+  knexInstance
+    .select('id', 'name', 'price', 'date_added', 'checked', 'category')
+    .from('shopping_list')
+    .where('date_added','>',
+      knexInstance.raw('now() - \'?? days\':: INTERVAL', dayAgo)
+    )
+    .then(result => {
+      console.log(result);
+    });
+}
+
+itemsAddedAfterDate(3);
+
+function totalCostForEachCategory(){
+  knexInstance
+    .select( 'category')
+    .sum('price')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log(result);
+    });
+}
+
+totalCostForEachCategory();
